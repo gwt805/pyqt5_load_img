503e0e0231541bfa715e18279e2c21ce9c46ceae
gwt805 <1973735972@qq.com>
2024-12-04 00:13:34 +0800

    update style

--- README.md
+++
@@ -1,4 +0,0 @@
-<div  align="center"><img src="./demo.jpg"></div>
-
-## 使用教程
-    > python app.py
--- app.py
+++ app.py
@@ -1,95 +1,134 @@
-import numpy as np
-import cv2, sys, os
+import os, sys
 from PyQt5.QtCore import Qt
-from __init__ import __version__
-from mainui import MainUI
-from PyQt5.QtGui import  QPixmap, QImage
-from PyQt5.QtWidgets import QApplication, QFileDialog, QMessageBox, QGraphicsPixmapItem
+from PyQt5 import QtWidgets
+from natsort import natsorted
+from mainui import Ui_MainWindow
+from PyQt5.QtNetwork import QLocalServer, QLocalSocket
+from PyQt5.QtGui import  QPixmap, QStandardItemModel, QStandardItem
+from PyQt5.QtWidgets import QFileDialog, QGraphicsPixmapItem, QMessageBox
 
-class Select:
+class AppImageView(Ui_MainWindow):
     def __init__(self):
-        self.mainWindow = MainUI()
-        self.mainWindow.setupUi()
+        super().__init__()
+        self.setupUi()
+        self.image_extensions = ('.png', '.jpg', '.jpeg', '.svg', '.webp', '.ico', '.icon', '.gif', '.bmp', '.tiff')
+        self.action_dir.triggered.connect(self.get_imgs)
+        self.img_idx = 0
+        self.img_list = []
+        self.btn_prev.clicked.connect(lambda: self.image_option("prev"))
+        self.btn_next.clicked.connect(lambda: self.image_option("next"))
+        self.btn_xuanzhuan.clicked.connect(self.rotate_img)
+        self.img_idx_input.textChanged.connect(self.input_img_idx)
+        self.label = QtWidgets.QLabel()
+        self.statusBar().addWidget(self.label)
+        self.model = QStandardItemModel()
 
-        self.image_path = ""
-        self.image_list = []
-        self.image_idx = 0
-
-        self.mainWindow.closeEvent = self.window_close
-        self.mainWindow.action_open_img_dir.triggered.connect(self.open_img_dir)
-        self.mainWindow.action_version.triggered.connect(self.menu_version)
-        self.mainWindow.btn_prev.clicked.connect(lambda: self.image_option("prev"))
-        self.mainWindow.btn_next.clicked.connect(lambda: self.image_option("next"))
-
-        self.mainWindow.show()
-
-    def open_img_dir(self):
-        tmp_path = QFileDialog.getExistingDirectory(self.mainWindow, "选择图片所在文件夹")
-        if tmp_path:
-            self.reset_param()
-            self.image_path = tmp_path
-            self.image_list = [im for im in os.listdir(self.image_path) if im.endswith(('.jpg', '.png', '.jpeg', '.bmp', '.tif', '.tiff', '.gif', '.webp', '.jfif'))]
-            # self.image_list = sorted(self.image_list, key=lambda x: int(x.split('.')[0]))
-            self.image_list.sort()
-            self.mainWindow.label_img_idx_total.setText(str(len(self.image_list)))
-            self.img_show("first")
+        self.show()
     
-    def img_show(self, option):
-        if self.image_path and self.image_list:
-            try:
-                frame = QPixmap(os.path.join(self.image_path, self.image_list[self.image_idx]))
-                self.mainWindow.graphicsView.image = frame
-                self.mainWindow.graphicsView.image_item = QGraphicsPixmapItem(frame)
+    def get_imgs(self):
+        self.path = QFileDialog.getExistingDirectory(self, "选择图片所在文件夹")
+        if self.path:
+            self.label.setText(f"当前目录: {self.path}")
+            self.img_list = self.find_image_files()
+            self.img_list = natsorted(self.img_list)
+            if self.img_list:
+                self.img_idx_now.setText("1")
+                self.img_idx_total.setText(str(len(self.img_list)))
+            self.img_list_show()
+            self.img_show("first")
 
-                if option == "first":
-                    self.mainWindow.graphicsView.loadImage()
-                if option == "update":
-                    self.mainWindow.graphicsView.update_image()
-                self.mainWindow.label_img_idx_now.setText(f"{self.image_idx + 1}")
-                self.mainWindow.label_img_name.setText(self.image_list[self.image_idx])
-            except Exception as e:
-                self.showMsg("错误提示", f"err: {str(e)}")
+    def find_image_files(self):
+        image_files = []
+        for root, dirs, files in os.walk(self.path):
+            for file in files:
+                if file.lower().endswith(self.image_extensions):
+                    image_files.append(os.path.join(root, file))
+        return image_files
 
-    def image_option(self, option: str):
-        if self.image_path and self.image_list:
-            if option == "prev":
-                if self.image_idx - 1 >= 0:
-                    self.image_idx -= 1
+    def img_list_show(self):
+        for it in self.img_list:
+            item = QStandardItem(it)
+            item.setToolTip(it)
+            self.model.appendRow(item)
+        self.listView.setModel(self.model)
+        self.listView.selectionModel().selectionChanged.connect(self.img_list_select)
 
-            if option == "next":
-                if self.image_idx < len(self.image_list) - 1:
-                    self.image_idx += 1
+    def input_img_idx(self):
+        if self.img_idx_input.value() <= 0:
+            self.img_idx_input.setValue(1)
+            self.img_idx = 0
+        elif self.img_idx_input.value() > len(self.img_list):
+            self.img_idx_input.setValue(len(self.img_list))
+            self.img_idx = len(self.img_list) - 1
+        else:
+            self.img_idx = int(self.img_idx_input.text()) - 1
             self.img_show("update")
 
-    def reset_param(self):
-        self.image_path = ""
-        self.image_list = []
-        self.image_idx = 0
+    def img_list_select(self, selected, deselected):
+        if selected.indexes():
+            self.img_idx = selected.indexes()[0].row()
+            self.img_show("update")
 
-    def window_close(self, event):
-        event.ignore()
-        self.mainWindow.setWindowFlags(Qt.SplashScreen | Qt.FramelessWindowHint) # 最小化到托盘
+    def img_show(self, option):
+        if self.img_list:
+            try:
+                self.img_name.setText(f" {self.img_list[self.img_idx]}({self.img_idx + 1}/{len(self.img_list)}) ")
+                frame = QPixmap(self.img_list[self.img_idx])
+                self.graphicsView.image = frame
+                self.graphicsView.image_item = QGraphicsPixmapItem(frame)
+                if option == "first":
+                    self.graphicsView.loadImage()
+                if option == "update":
+                    self.graphicsView.update_image()
+            except Exception as e:
+                self.showMsg("错误提示", f"err: {str(e)}")
+        else:
+            self.img_name.setText("未找到图片")
+            self.graphicsView.scene.clear()
+            self.graphicsView.image_item = None
+            return None
 
-    def menu_version(self):
-        reply = QMessageBox(QMessageBox.Information, "版本", __version__, QMessageBox.NoButton, self.mainWindow)
-        reply.addButton("确定", QMessageBox.YesRole)
-        reply.addButton("取消", QMessageBox.NoRole)
-        reply.exec_()
+    def rotate_img(self):
+        if self.img_list:
+            self.graphicsView.rotate_image()
+
+    def image_option(self, option: str):
+        if self.img_list:
+            if option == "prev":
+                if self.img_idx - 1 >= 0:
+                    self.img_idx -= 1
+
+            if option == "next":
+                if self.img_idx < len(self.img_list) - 1:
+                    self.img_idx += 1
+            self.img_show("update")
 
     def showMsg(self, title: str, msg: str):
-        reply = QMessageBox(QMessageBox.Warning, title, msg, QMessageBox.NoButton, self.mainWindow)
+        reply = QMessageBox(QMessageBox.Warning, title, msg, QMessageBox.NoButton, self)
         reply.addButton("确定", QMessageBox.YesRole)
         reply.addButton("取消", QMessageBox.NoRole)
         reply.exec_()
 
 def main():
-    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
-    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
-    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
-    app = QApplication(sys.argv)
-    app.setQuitOnLastWindowClosed(False)
-    select = Select()
-    sys.exit(app.exec_())
+    QtWidgets.QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QtWidgets.QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QtWidgets.QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+    app = QtWidgets.QApplication(sys.argv)
+    server_name = "pyqt5-image-viewer"
+    socket = QLocalSocket()
+    
+    socket.connectToServer(server_name)
+    if socket.waitForConnected(1000):
+        app.quit()
+    else:
+        local_server = QLocalServer()
+        local_server.listen(server_name)
+        app.setQuitOnLastWindowClosed(False)
+        aiv = AppImageView()
+        sys.exit(app.exec_())
 
 if __name__ == "__main__":
+    try:
         main()
+    except Exception as e:
+        print(f"err: {str(e)}")
